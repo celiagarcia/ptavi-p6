8610bafce51b41b5d5a142f44d3915c208e4fb89
celia garcia fernandez <cgarcia@iota29.aulas.gsyc.es>
2014-11-21 18:15:50 +0100

    servidor con rtp

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import SocketServer
 import sys
-
+import os
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
@@ -19,12 +19,19 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             if line != "":
+            
                 print "El cliente nos manda " + line
                 
+                linea = line.split()
                 Metodo = line.split()[0]
                 Sip = 'SIP/2.0 '
+                # 400
+                if len(linea) != 3 or linea[2] != 'SIP/2.0':
+                    envio = Sip + '400 Bad Request' + '\r\n'
+                    self.wfile.write(envio)
+                
                 # INVITE
-                if Metodo == 'INVITE':
+                elif Metodo == 'INVITE':
                     envio = Sip + '100 TRYING' + '\r\n'
                     envio += Sip + '180 RING' + '\r\n'
                     envio += Sip + '200 OK' + '\r\n'
@@ -37,7 +44,13 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                     
                 # ACK
                 elif Metodo == 'ACK':
-                    print 'Comienza RTP'
+                
+                    print 'Comienza RTP \r\n' 
+                    # aEjecutar es un string con lo que se ha de ejecutar en la shell
+                    aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + fichero_audio
+                    print 'Vamos a ejecutar ' + aEjecutar
+                    os.system(aEjecutar)
+                # 405
                 else:
                     envio = Sip + '405 Method Not Allowed'
                     self.wfile.write(envio)
@@ -53,16 +66,13 @@ if __name__ == "__main__":
         # Puerto
         PORT = int(sys.argv[2])
         # IP
-        IP = sys.argv[1]
+        IP = str(sys.argv[1])
         # Fichero
-        Fich_audio = sys.argv[3]
+        fichero_audio = str(sys.argv[3])
     except ValueError:
         sys.exit('Usage: python server.py IP port audio_file')
     except IndexError:
         sys.exit('Usage: python server.py IP port audio_file')
-
-
-
 
 
     # Creamos servidor y escuchamos
