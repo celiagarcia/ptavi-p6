bc56c58b7407f9ed6154daca6c1d654107345299
celia garcia fernandez <cgarcia@iota29.aulas.gsyc.es>
2014-11-21 18:15:37 +0100

    cliente con rtp

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ Programa cliente que abre un socket a un servidor
 
 import socket
 import sys
+import os
 
 # Cliente UDP simple.
 
@@ -15,9 +16,11 @@ Metodos = ['INVITE', 'BYE']
 try:
     # Metodo
     METODO = sys.argv[1]
+    """
     if not METODO in Metodos:
         print 'Usage: python client.py method receiver@IP:SIPport'
         raise SystemExit 
+    """
     # Información del receptor
     INFOR = sys.argv[2]
     PORT = int(sys.argv[2].split('@')[1].split(':')[1])
@@ -45,11 +48,13 @@ try:
     my_socket.send(LINE + '\r\n')
     data = my_socket.recv(1024)
 
-    print 'Recibido -- ' + '\r\n', data #¡¡¡¡HE AÑADIDO TO \R\N!!!!!
+    print 'Recibido -- ' + '\r\n', data
+    
+    datos =  data.split()
     
     # Procesando las respuestas
     Sip = 'SIP/2.0 '
-    if data == Sip + '100 TRYING' + '\r\n' + Sip + '180 RING' + '\r\n' + Sip + '200 OK' + '\r\n':
+    if datos[1] == '100' and datos[4] == '180' and datos[7] == '200':
         LINE = 'ACK SIP:' + DIRECCION + ' SIP/2.0'
         print "Enviando: " + LINE
         my_socket.send(LINE + '\r\n')
@@ -57,21 +62,16 @@ try:
         
         print 'Comienza RTP'
         
+    """    
+    elif datos[1] = '200':
         
+    elif datos[1] == '400':
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    elif datos[1] == '405':
+        
     print "Terminando socket..."
-
+    """
+    
     # Cerramos todo
     my_socket.close()
     print "Fin."
